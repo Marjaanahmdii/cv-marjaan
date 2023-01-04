15117e12492d1fcc55f0b0b824a11261d9df1259
Administrator <marjaanahmdi.1994@gmail.com>
2023-01-05 02:03:45 +0400

    vommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import moon from './moon.png';
+import React from 'react';
+import fleekLogo from './fleek-logo.png';
+import reactLogo from './react-logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={moon} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={fleekLogo} className="Fleek-logo" alt="fleek-logo" />
+        <span className="Big-plus">+</span>
+        <img src={reactLogo} className="React-logo" alt="react-logo" />
+        <p>
+          React App deployed on IPFS through Fleek.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.fleek.co"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a> */}
+          Learn More About Fleek!
+        </a>
       </header>
     </div>
   );
